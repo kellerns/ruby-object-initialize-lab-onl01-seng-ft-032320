eeebb86ad3433792b6a80db42b7c7041e07804d5
Nicholas Keller <nic.keller328@gmail.com>
2020-04-02 17:40:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,11 @@
 class Dog
   def initialize(name, breed)
     @name = name
-    @breed = breed
     if block_given?
       @breed = breed
-      @breed = "Mutt"  
+    else
+      @breed = "Mutt"
+    end
   
   
   
